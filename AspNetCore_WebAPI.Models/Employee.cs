be4ef99acc6d6a56741b2321df337fc4a55d5e52
Sneha Shah <sneha@nthstar.com.au>
2023-03-06 12:15:51 +0530

    Initial Commit

---
+++ AspNetCore_WebAPI.Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace AspNetCore_WebAPI.Models
+{
+  public class Employee
+  {
+    public string EmployeeID { get; set; }
+    public string EmployeeName { get; set; }
+    public string EmployeeAddress { get; set; }
+    public string EmployeeMobile { get; set; }
+    public string EmployeeEmail { get; set; }
+  }
+}
